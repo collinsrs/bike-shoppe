eaf39d1595b085af9961eadec9af9a7357df9fed
Rishi Collins <67651571+collinsrs@users.noreply.github.com>
2022-08-08 00:33:57 -0400

    Add Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,13 @@ import 'styles/global.css';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
 import { SessionProvider } from 'next-auth/react';
+import { GoogleAnalytics } from 'nextjs-google-analytics';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
       <ThemeProvider attribute="class">
+        <GoogleAnalytics strategy="lazyOnload" />
         <Component {...pageProps} />
       </ThemeProvider>
     </SessionProvider>
