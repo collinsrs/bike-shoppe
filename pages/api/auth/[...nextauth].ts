c3a4095cad72507956ed26df9947f362919704d0
Rishi Collins <67651571+collinsrs@users.noreply.github.com>
2022-08-23 16:05:03 -0400

    Change provider to Auth0

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,11 +1,12 @@
 import NextAuth from 'next-auth';
-import GitHubProvider from 'next-auth/providers/github';
+import Auth0Provider from "next-auth/providers/auth0";
 
 export default NextAuth({
   providers: [
-    GitHubProvider({
-      clientId: 'ef1273a0b63bccbf88d2',
-      clientSecret: '67a2e4ed028ce5de84f832427439a8bde79ae4d2',
+    Auth0Provider({
+      clientId: 'A8MUM7f4Ds0lU93RqUDA3OgdTabWTcAH',
+      clientSecret: 'tG3dGVnnPf3U2mfmoBiQJFr5GXRmIS_4YghsavYYpbB_MsYyi2W5nAq0s3oQLL5P',
+      issuer: 'dev-4o4acxx0.us.auth0.com'
     })
   ]
 });
