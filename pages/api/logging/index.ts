f101812428c624836090efbfb1672f1d40fac8fb
Rishi Collins <67651571+collinsrs@users.noreply.github.com>
2022-08-25 18:28:37 -0400

    Request ID Logging

--- pages/api/logging/index.ts
+++ pages/api/logging/index.ts
@@ -1,15 +1,16 @@
 import prisma from '../../../lib/prisma';
-import { uuid } from 'uuidv4';
+import {uuid} from 'uuidv4';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 
 export default async function logRemote(req: NextApiRequest, res: NextApiResponse) {
+    const requestId = uuid();
     const remoteIP: any = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
     const remoteCity: any = req.headers['x-vercel-ip-city'] || 'Unknown or Development';
     const remoteLocale: any = req.headers['x-vercel-ip-country-region'] || 'Unknown or Development';
     const remoteCountry: any = req.headers['x-vercel-ip-country'] || 'Unknown or Development';
     const remoteTimeZone: any = req.headers['x-vercel-ip-timezone'] || 'Unknown or Development';
-    res.setHeader('ssc-request-id', uuid());
+    res.setHeader('ssc-request-id', requestId);
     const refererSlug: any = req.query.ref;
     const result = await prisma.request.create({
         data: {
@@ -31,6 +32,7 @@ export default async function logRemote(req: NextApiRequest, res: NextApiRespons
             'requestMethod', 'post',
             'requestStatus', '201 Created',
             'timestamp', new Date(),
+            'requestId', requestId,
         ]
     }
     res.status(201).json(jsonData);
