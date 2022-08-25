0e1c1d98f9fe139d00958db1df251384aa2e1b79
Rishi Collins <67651571+collinsrs@users.noreply.github.com>
2022-08-25 18:36:00 -0400

    Bug fix p2

--- pages/api/logging/index.ts
+++ pages/api/logging/index.ts
@@ -7,7 +7,6 @@ export default async function logRemote(req: NextApiRequest, res: NextApiRespons
     const requestId = uuid();
     const remoteIP: any = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
     const city: any = req.headers['x-vercel-ip-city'] || 'Unknown or Development';
-    const locale: any = req.headers['x-vercel-ip-country-region'] || 'Unknown or Development';
     const country: any = req.headers['x-vercel-ip-country'] || 'Unknown or Development';
     const remoteTimeZone: any = req.headers['x-vercel-ip-timezone'] || 'Unknown or Development';
     res.setHeader('ssc-request-id', requestId);
@@ -17,7 +16,6 @@ export default async function logRemote(req: NextApiRequest, res: NextApiRespons
             slug: refererSlug,
             requestMethod: 'get',
             remoteUser: remoteIP,
-            remoteLocale: locale,
             remoteCountry: country,
             remoteCity: city,
             statusCode: 200,
