7427d0d7cefd2686551e7629198ee1880163dd8b
Rishi Collins <67651571+collinsrs@users.noreply.github.com>
2022-08-25 18:31:52 -0400

    Bug fix

--- pages/api/logging/index.ts
+++ pages/api/logging/index.ts
@@ -6,9 +6,9 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 export default async function logRemote(req: NextApiRequest, res: NextApiResponse) {
     const requestId = uuid();
     const remoteIP: any = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    const remoteCity: any = req.headers['x-vercel-ip-city'] || 'Unknown or Development';
-    const remoteLocale: any = req.headers['x-vercel-ip-country-region'] || 'Unknown or Development';
-    const remoteCountry: any = req.headers['x-vercel-ip-country'] || 'Unknown or Development';
+    const city: any = req.headers['x-vercel-ip-city'] || 'Unknown or Development';
+    const locale: any = req.headers['x-vercel-ip-country-region'] || 'Unknown or Development';
+    const country: any = req.headers['x-vercel-ip-country'] || 'Unknown or Development';
     const remoteTimeZone: any = req.headers['x-vercel-ip-timezone'] || 'Unknown or Development';
     res.setHeader('ssc-request-id', requestId);
     const refererSlug: any = req.query.ref;
@@ -17,9 +17,9 @@ export default async function logRemote(req: NextApiRequest, res: NextApiRespons
             slug: refererSlug,
             requestMethod: 'get',
             remoteUser: remoteIP,
-            remoteLocale: remoteLocale,
-            remoteCountry: remoteCountry,
-            remoteCity: remoteCity,
+            remoteLocale: locale,
+            remoteCountry: country,
+            remoteCity: city,
             statusCode: 200,
             userAgent: req.headers['user-agent'],
             timestamp: new Date(),
