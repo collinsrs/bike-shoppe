7a4c0a3294add34afb526795f6e2dcfe26c7498b
Rishi Collins <67651571+collinsrs@users.noreply.github.com>
2022-11-02 15:00:37 -0400

    Added Vercel Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
 import { SessionProvider } from 'next-auth/react';
 import { GoogleAnalytics } from 'nextjs-google-analytics';
+import { Analytics } from '@vercel/analytics/react';
 
 const gaMeasurementId = 'G-XKSNX4TTFJ';
 
@@ -11,8 +12,9 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
       <ThemeProvider attribute="class">
-        <GoogleAnalytics gaMeasurementId={gaMeasurementId} strategy="lazyOnload" />
         <Component {...pageProps} />
+        <Analytics />
+        <GoogleAnalytics gaMeasurementId={gaMeasurementId} strategy="lazyOnload" />
       </ThemeProvider>
     </SessionProvider>
   );
